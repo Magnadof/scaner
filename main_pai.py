da10775f04e3814e36c0a02216da2a952636b520
magnadof <higoralves550@gmail.com>
2023-12-28 01:35:59 -0300

    arquivo pai feito

---
+++ main_pai.py
@@ -0,0 +1,16 @@
+def salvar_caminhos_em_arquivo(entry, exit, rir, nome_arquivo_saida):
+    with open(nome_arquivo_saida, 'w') as arquivo_saida:
+        arquivo_saida.write(f'entry: {entry}\n')
+        arquivo_saida.write(f'exit: {exit}\n')
+        arquivo_saida.write(f'rir: {rir}\n')
+
+    print(f'Os caminhos foram salvos no arquivo: {nome_arquivo_saida}')
+
+# Exemplo de uso
+entry = input('\033[93mCopy and paste the input path of the files:\n\n\033[0m')
+exit = input('\033[93mAdd to Output Folder:\n\n\033[0m')
+rir = input('\033[93mCopy and paste the path of the RIR folder:\n\n\033[0m')
+nome_arquivo_saida = 'paths'
+
+salvar_caminhos_em_arquivo(entry, exit, rir, nome_arquivo_saida)
+
